6958eda28d84bc90c8507ce1dd83daa0b1335616
fernandogelin <fernandogelin@gmail.com>
2013-11-06 17:16:29 -0500

    added protein translation code

--- reverse_complement.py
+++ reverse_complement.py
@@ -1,15 +1,3 @@
-# In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'. Given a nucleotide p, we denote its complementary nucleotide as p. The reverse complement of a string Pattern = p1…pn is the string Pattern = pn … p1 formed by taking the complement of each nucleotide in Pattern, then reversing the resulting string.
-
-# For example, the reverse complement of Pattern = "GTCA" is Pattern = "TGAC".
-
-# Reverse Complement Problem
-
-# Reverse complement a nucleotide pattern.
-
-# Given: A DNA string Pattern.
-
-# Return: Pattern, the reverse complement of Pattern.
-
 seq = raw_input("Enter sequence:")
 def reverse_complement (seq):
   basecomplement = {'A':'T', 'C':'G', 'T':'A', 'G':'C'}  # lookup table of complements
